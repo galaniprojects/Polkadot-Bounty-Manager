751f6e8505b3ddb73e275a0940340e90bb08d743
BlertaGalani <98878525+BlertaGalani@users.noreply.github.com>
2024-08-12 16:03:33 +0200

    add mobile responsiveness to all three screens

--- src/components/bounty-setup/BountyCreation.svelte
+++ src/components/bounty-setup/BountyCreation.svelte
@@ -1,7 +1,10 @@
 <script>
 	let success = false;
-	let bountyId = '#88';
-	let bountyTitle = 'Community Event Activity Bounty';
+
+	let bounty = {
+		id: '#88',
+		title: 'Community Event Activity Bounty'
+	};
 
 	function submit() {
 		success = !success;
@@ -9,18 +12,25 @@
 </script>
 
 <div>
-	<div class="top-bar">
+	<div class="top-bar flex justify-between">
 		{#if !success}
-			<input class="rounded-sm bg-gray-100 w-96 pl-3 pt-1" placeholder="Give your Bounty a title" />
+			<input
+				class="rounded-md bg-gray-100 w-1/2 pl-3 pt-1"
+				placeholder="Give your Bounty a title"
+			/>
+			<p class="text text-sm mt-1.5 text-white">
+				<span class="opacity-50">Need more information about the Bounty Setup process? </span>
+				<a href="#moreinfo">Tap here</a>
+			</p>
 		{:else}
-			<p class="text-white">{`${bountyId} ${bountyTitle}`}</p>
+			<p class="text-white">{`${bounty.id} ${bounty.title}`}</p>
 		{/if}
 	</div>
 
 	{#if success}
 		<div class="content">
 			<p>
-				{`${bountyId} ${bountyTitle}`}
+				{`${bounty.id} ${bounty.title}`}
 				created successfully!
 				<br /><br />
 				You can either proceed to 2 Referendum <br />
@@ -30,29 +40,28 @@
 
 			<div class="mt-5 flex">
 				<button class="button-cancel mr-5">LIST</button>
-				<button on:click={() => submit()} class="button-active">PROCEED</button>
+				<button on:click={submit} class="button-active">PROCEED</button>
 			</div>
 		</div>
 	{:else}
 		<div class="grid content-between content">
 			<div>
-				<p class="text-sm">Bounty value</p>
+				<p class="text-xs">Bounty value</p>
 				<div class="flex mt-2">
-					<input class="border pt-1 pl-2 w-64 rounded-s bg-white mr-2" placeholder="0.000.0000" />
-					<p class="mt-1">DOT</p>
+					<input class="border pt-1 pl-2 w-1/4 rounded-md bg-white" placeholder="0.000.0000" />
 				</div>
-				<hr class="border-white mt-5 mb-1 w-96" />
+				<hr class="border-white mt-5 mb-1 w-1/2" />
 
-				<p class="text-sm">Bounty description</p>
+				<p class="text-xs">Bounty description</p>
 				<textarea
-					class="border pt-1 pl-2 md:w-96 h-32 rounded-md bg-white mt-2e mr-2 mt-2"
+					class="border pt-1 pl-2 w-1/2 h-28 rounded-md bg-white mt-2e mr-2 mt-2"
 					placeholder="Your Bounty description goes here"
-				></textarea>
+				/>
 
-				<hr class=" border-white mt-5 mb-2 w-96" />
-				<p class="text-sm mb-2">Submit with account</p>
+				<hr class=" border-white mt-5 mb-2 w-1/2" />
+				<p class="text-xs mb-2">Submit with account</p>
 
-				<select class="border w-64" name="accounts" id="accounts">
+				<select class="border pt-1 pl-2 w-1/4 rounded-md" name="accounts" id="accounts">
 					<option value="alice">Alice</option>
 					<option value="bob">Bob</option>
 				</select>
@@ -67,9 +76,9 @@
 					</section>
 				</div>
 			</div>
-			<div class="flex">
+			<div class="buttons flex">
 				<button class="button-cancel mr-5">CANCEL</button>
-				<button on:click={() => submit()} class="button-active">SUBMIT</button>
+				<button on:click={submit} class="button-active">SUBMIT</button>
 			</div>
 		</div>
 	{/if}
