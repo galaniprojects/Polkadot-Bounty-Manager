bd21d5860afba28600f15248fcb96f2c2a1dc791
Abdulrahim Al Methiab <abdul@kilt.io>
2024-08-14 10:34:40 +0200

    format

--- src/components/bounty-setup/BountySetupTab.svelte
+++ src/components/bounty-setup/BountySetupTab.svelte
@@ -7,7 +7,7 @@
 <div class="tab-bar rounded-md w-full h-full">
 	<button
 		class="text-2xl flex justify-center items-center w-full h-full pt-2
-    {activeTab !== tabName ? ' text-gray-400 border-b-transparent border-b-4 ' : ' text-white border-b-accent border-b-4'}"
+    {activeTab !== tabName ? 'text-gray-400 border-b-transparent border-b-4' : 'text-white border-b-accent border-b-4'}"
 		on:click={() => {
 			bountySetupTabClicked(tabName);
 		}}
