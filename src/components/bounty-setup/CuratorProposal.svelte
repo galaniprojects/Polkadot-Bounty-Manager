0b19ca6012ae08704d8476be63463d070a236def
Abdulrahim Al Methiab <abdul@kilt.io>
2024-08-29 13:16:38 +0200

    fix merge issues

--- src/components/bounty-setup/CuratorProposal.svelte
+++ src/components/bounty-setup/CuratorProposal.svelte
@@ -161,7 +161,6 @@
 					<p class="text-xs my-1">Curator Address</p>
 					<input
 						bind:value={curatorAddress}
-						class="border w-1/2 pt-1 pl-2 rounded-md bg-white mr-2"
 						class="border border-borderColor w-1/2 pt-1 pl-2 rounded-md bg-white mr-2"
 						placeholder="5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty"
 						on:input={inputChange}
@@ -169,11 +168,9 @@
 					<p class="text-xs my-1">Curator Fee</p>
 					<input
 						bind:value={curatorFee}
-						class="border pt-1 pl-2 rounded-md bg-white mr-2 w-1/3"
-						placeholder="100.0000"
+						placeholder="100"
 						on:input={inputChange}
 						class="border border-borderColor pt-1 pl-2 rounded-md bg-white mr-2 w-1/3"
-						placeholder="100.0000"
 					/>
 					<p class="text-xs mt-1">(total Bounty value: 100.000.0000 DOT)</p>
 				</div>
