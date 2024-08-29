4a86ec6ebbeb455aa663ed49c9739aa79a00bed7
Abdulrahim Al Methiab <abdul@kilt.io>
2024-08-29 15:12:10 +0200

    trigger CI

--- src/stores.ts
+++ src/stores.ts
@@ -2,6 +2,7 @@ import { writable } from 'svelte/store';
 import type {InjectedAccountWithMeta} from '@polkadot/extension-inject/types'
 
 export const isLoggedIn = writable(false);
+
 const addresses: InjectedAccountWithMeta[] = [];
 export const loggedAccounts = writable(addresses);
 let account: InjectedAccountWithMeta | undefined;
