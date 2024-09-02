e453a3948cd8fa23ef53dcc9217097792d4119c9
Gerawork Aynekulu <ggera@users.noreply.github.com>
2024-09-02 10:10:17 +0200

    Merge develop into test (#115)
    * ci: deployment workflows (#91)
    
    * add ci
    
    * fix typo
    
    * add rest PR
    
    * Update test-pull-request
    
    * Create main.yml
    
    * ci:rename
    
    * upgrade action
    
    * introduce ci branch
    
    * replace pr auto-merge
    
    * replace action with cli
    
    * fix typo
    
    * [WIP] Bounty Setup Functionalities (#107)
    
    * Create curator action accordion content (#96)
    
    ---------
    
    Co-authored-by: Abdulrahim Al Methiab <31316147+abdulmth@users.noreply.github.com>
    Co-authored-by: BlertaAtGalaniprojects <102804060+BlertaAtGalaniprojects@users.noreply.github.com>

---
+++ src/components/curator-actions/CuratorItem.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let index;
+	export let curator;
+</script>
+
+<div class="flex items-center justify-between py-1">
+	<div class="flex items-center">
+		<span class="mr-3">{index + 1}.</span>
+		<span class="curator-name text-sm truncate">{curator.name}</span>
+	</div>
+	<button class="ml-3 p-1">
+		<span class="material-symbols-outlined text-accent">content_copy</span>
+	</button>
+</div>
+
+<style>
+	.curator-name {
+		font-family: 'PT Mono';
+	}
+</style>
