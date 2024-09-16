8828eef2498248d0a4a2ba8dad373a720b827d9a
Abdulrahim Al Methiab <abdul@kilt.io>
2024-09-16 15:16:58 +0200

    refactor

---
+++ src/utils/loading-screen.ts
@@ -0,0 +1,35 @@
+import { loadingDialogState } from '../stores';
+import type { LoadingDialogState } from '../types/loaing-screen';
+
+export function showLoadingDialog(title: string) {
+  const state: LoadingDialogState = {
+    open: true,
+    status: 'loading',
+    title: title,
+    successMessage: '',
+    errorMessage: ''
+  };
+  loadingDialogState.set(state);
+}
+
+export function showErrorDialog(message: string) {
+  const state: LoadingDialogState = {
+    open: true,
+    status: 'error',
+    title: "Error",
+    successMessage: '',
+    errorMessage: message
+  };
+  loadingDialogState.set(state);
+}
+
+export function showSuccessDialog(title: string, message: string) {
+  const state: LoadingDialogState = {
+    open: true,
+    status: 'success',
+    title: title,
+    successMessage: message,
+    errorMessage: ''
+  };
+  loadingDialogState.set(state);
+}
