117760e279486c3c2ea94f8d4e36fced320d0909
BlertaGalani <98878525+BlertaGalani@users.noreply.github.com>
2024-09-20 12:03:40 +0200

    update desktop UI curator action screen

--- src/components/curator-actions/AccordionItem.svelte
+++ src/components/curator-actions/AccordionItem.svelte
@@ -55,7 +55,7 @@
 	}
 </script>
 
-<div class="bg-secondary border border-accent rounded-md my-6">
+<div class="bg-curatorMainBackground rounded-md my-6">
 	<!-- Mobile design-->
 	<div class="lg:hidden">
 		<div
@@ -106,7 +106,7 @@
 				</section>
 
 				<div class="flex justify-between">
-					<div class="grid">
+					<div class="grid max-w-52">
 						<section>
 							<p class="text-xs">Remaining balance</p>
 							<p><span>1,230.9800</span> DOT</p>
@@ -135,7 +135,7 @@
 					</div>
 				</div>
 				<hr class="border-white opacity-30 mt-6 mb-3" />
-				<section class="flex justify-between items-center">
+				<section class="flex justify-between items-center w-64">
 					<p class="text-xs">Curator ({curators.length})</p>
 					<button
 						class="material-symbols-outlined flex justify-center items-center bg-accent text-white rounded-full w-6 h-6 text-lg ml-3"
@@ -148,7 +148,7 @@
 						{/if}
 					</button>
 				</section>
-				<div class="flex justify-between my-3">
+				<div class="flex justify-between my-3 w-64">
 					<p class="main-curator">13iTojfEzgwrKzvEkuAmxrtm</p>
 					<button class="material-symbols-outlined text-accent w-6 h-6">content_copy</button>
 				</div>
@@ -222,7 +222,7 @@
 				<ChildBountyCard
 					id="8887"
 					title="Marketing Allocation"
-					status="payout pending"
+					status="claimed"
 					value="47,370.0000 DOT"
 					curatorFee="1.9857 DOT"
 					subCurator="7H1RjCf3...Ngcq"
@@ -236,37 +236,207 @@
 	</div>
 
 	<!-- Desktop design -->
-	<div
-		class="lg:w-[960px] xl:w-[1200px] 2xl:w-[1280px] hidden lg:flex justify-between items-center max-h-20 pl-10 pr-6"
-	>
-		<div class="text-white flex">
-			<span class="text-2xl">#89</span>
-			<span class="text-2xl ml-3">Community Event Activities Bounty 02</span>
-		</div>
+	<div class="lg:w-[960px] xl:w-[1300px] 2xl:w-[1280px] hidden lg:grid">
+		<!-- Header Section -->
+		<section class="text-white max-h-20 px-10 my-4">
+			<div class="flex justify-between">
+				<span class="text-2xl">#{`${bounty.id}`} {`${bountyDetails.title}`}</span>
+				<div class="flex gap-2">
+					<button class="w-6 h-6"><LogoTreasuryIcon /></button>
+					<button class="w-6 h-6"><LogoPolkassembly /></button>
+					<button class="w-6 h-6"><LogoSubscan /></button>
+				</div>
+			</div>
+			<div class="flex justify-start text-xs pl-12">
+				<section class="flex space-x-1 mr-1">
+					<p>Proposer:</p>
+					<p>JDoe_Chain</p>
+				</section>
+				<section class="flex space-x-1 ml-16">
+					<p>Created:</p>
+					<p>8th August 2024</p>
+				</section>
+				<section class="flex space-x-1 ml-40">
+					<p>Expiration date:</p>
+					<p>27th September 2024</p>
+				</section>
+			</div>
+		</section>
 
-		<div class="flex justify-between items-center">
-			<button class="info-svg w-6 h-6 m-2"><InfoSvg /></button>
-			<button
-				class="curator-btn text-accent border-accent border min-w-fit rounded-md font-bold px-3 ml-2 mr-5"
-				>ACCEPT CURATOR
-			</button>
+		<!-- Content Section -->
 
-			<button class="info-svg w-6 h-6 m-2"><InfoSvg /></button>
-			<button
-				class="optin-btn text-darkgray border-darkgray border min-w-fit rounded-md font-bold px-9 ml-2 mr-6"
-				>OPT-IN
-			</button>
-			<button
-				class="material-symbols-outlined border-white text-white border-2 rounded-full p-2 w-6 h-6 text-xl flex justify-center items-center"
-				on:click={handleToggleClick}
-			>
-				{#if expanded}
-					keyboard_arrow_up
-				{:else}
-					keyboard_arrow_down
-				{/if}
-			</button>
-		</div>
+		{#if !expanded}
+			<div class="p-3 pt-6 flex justify-start text-white">
+				<div class="flex justify-start ml-7">
+					<div class="grid gap-2">
+						<section class="flex">
+							<div>
+								<p class="text-xs">Remaining balance</p>
+								<p class="text-2xl"><span>1,230.9800</span> DOT</p>
+							</div>
+							<div>
+								<section class="ml-16 w-72">
+									<p class="text-xs">Curator ({curators.length})</p>
+									<div class="flex items-center space-x-2">
+										<p class="main-curator">13iTojfEzgwrKzvEkuAmxrtm</p>
+										<button class="material-symbols-outlined w-6 h-6"
+											>content_copy</button
+										>
+
+										<button
+											class="material-symbols-outlined flex justify-center items-center border border-white text-white rounded-full w-6 h-6 text-lg ml-3"
+											on:click={handleCuratorsToggleClick}
+										>
+											{#if curatorsExpended}
+												keyboard_arrow_up
+											{:else}
+												keyboard_arrow_down
+											{/if}
+										</button>
+									</div>
+									{#if curatorsExpended}
+										<div class="mt-1">
+											{#each curators as curator, index}
+												<CuratorItem {index} {curator} />
+											{/each}
+										</div>
+									{/if}
+								</section>
+							</div>
+							<div class="ml-2">
+								<p class="text-xs">Curator Fee</p>
+								<p class="text-md"><span>4.0057</span> DOT</p>
+							</div>
+						</section>
+						<section class="text-xs max-w-52">
+							<p>Description</p>
+							<p> This proposal stems from the need to enhance system upgrade testing procedures,
+								minimizing feature regression. Recent discussions in various channels, spurred by …
+							</p>
+						</section>
+					</div>
+				</div>
+				<section class="space-y-3 ml-44 ">
+					<div class="flex justify-end items-center">
+						<p class="mr-3 text-sm">Beneficiary Claim Form</p>
+						<button class="bg-accent rounded-md font-bold min-w-32 pt-1">ADD</button>
+						<button class="w-6 h-6 ml-2"><InfoSvg /></button>
+					</div>
+					<div class="flex justify-end items-center mr-8">
+						<p class="mr-3 text-sm">Award Bounty</p>
+						<button class="bg-accent rounded-md font-bold min-w-32 pt-1">READ FIRST</button>
+					</div>
+				</section>
+			</div>
+				<div class="bg-childBountyBackground p-3 m-3">
+					<section class="flex justify-start px-5 pb-6">
+						<div class="grid gap-2 w-[500px]">
+							<p class="text-xs">Child Bounties</p>
+							<p class="text-2xl">Child Bounties</p>
+							<div class="relative w-full h-10">
+								<input
+									class="border border-borderColor pt-1 w-full rounded-md bg-white pl-2"
+									placeholder="Bounty Search"
+								/>
+								<div
+									class="border border-accent absolute right-8 top-4 transform -translate-y-1/2 h-5"
+								></div> 
+
+								<svg
+									class="w-3 h-3 text-accent absolute right-3 top-4 transform -translate-y-1/2"
+									aria-hidden="true"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 20 20"
+								>
+									<path
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+									/>
+								</svg>
+							</div>
+						</div>
+						<div class="grid space-y-3 ml-72">
+							<div class="flex justify-end items-center py-3">
+								<p class="mr-3 text-md">New Child Bounty</p>
+								<button class="bg-accent text-white rounded-md font-bold pt-1 min-w-32 mr-3"
+									>ADD</button
+								>
+							</div>
+							<div class="flex justify-end items-center pb-3">
+								<p class="mr-3 text-md">Salary Child Bounties</p>
+								<button class="bg-accent text-white rounded-md font-bold pt-1 min-w-32 mr-3"
+									>ADD</button
+								>
+							</div>
+						</div>
+					</section>
+
+					<ChildBountyCard
+						id="8887"
+						title="Marketing Allocation"
+						status="created"
+						value="47,370.0000 DOT"
+						curatorFee="1.9857 DOT"
+						subCurator="7H1RjCf3...Ngcq"
+						beneficiary="Nodal_point_12"
+						dateCreated="MAY 09, 2024"
+						dateOfPayout="AUG 21, 2024"
+						timeUntilPayout="1d : 8h : 42min : 18sec"
+					/>
+					<ChildBountyCard
+						id="8887"
+						title="Marketing Allocation"
+						status="sub-curator proposed"
+						value="47,370.0000 DOT"
+						curatorFee="1.9857 DOT"
+						subCurator="7H1RjCf3...Ngcq"
+						beneficiary="Nodal_point_12"
+						dateCreated="MAY 09, 2024"
+						dateOfPayout="AUG 21, 2024"
+						timeUntilPayout="1d : 8h : 42min : 18sec"
+					/>
+					<ChildBountyCard
+						id="8887"
+						title="Bounty 2"
+						status="active"
+						value="47,370.0000 DOT"
+						curatorFee="1.9857 DOT"
+						subCurator="7H1RjCf3...Ngcq"
+						beneficiary="Nodal_point_12"
+						dateCreated="MAY 09, 2024"
+						dateOfPayout="AUG 21, 2024"
+						timeUntilPayout="1d : 8h : 42min : 18sec"
+					/>
+					<ChildBountyCard
+						id="8887"
+						title="Bounty 3"
+						status="awarded"
+						value="47,370.0000 DOT"
+						curatorFee="1.9857 DOT"
+						subCurator="7H1RjCf3...Ngcq"
+						beneficiary="Nodal_point_12"
+						dateCreated="MAY 09, 2024"
+						dateOfPayout="AUG 21, 2024"
+						timeUntilPayout="1d : 8h : 42min : 18sec"
+					/>
+					<ChildBountyCard
+						id="8887"
+						title="Bounty 4 very long title name for testing it"
+						status="claimed"
+						value="47,370.0000 DOT"
+						curatorFee="1.9857 DOT"
+						subCurator="7H1RjCf3...Ngcq"
+						beneficiary="Nodal_point_12"
+						dateCreated="MAY 09, 2024"
+						dateOfPayout="AUG 21, 2024"
+						timeUntilPayout="1d : 8h : 42min : 18sec"
+					/>
+				</div>
+		{/if}
 	</div>
 </div>
 {#if acceptCuratorRuleDialogOpened}
@@ -274,11 +444,6 @@
 {/if}
 
 <style>
-	.proposer-name,
-	.main-curator {
-		font-family: 'PT Mono';
-	}
-
 	hr {
 		box-shadow: 0 -1px 0 0 gray;
 	}
--- src/components/curator-actions/ChildBountyCard.svelte
+++ src/components/curator-actions/ChildBountyCard.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+	import LogoPolkassembly from '../svg/LogoPolkassembly.svelte';
+	import LogoSubscan from '../svg/LogoSubscan.svelte';
+	import LogoTreasuryIcon from '../svg/LogoTreasuryIcon.svelte';
+
 	export let id: string | number = '';
 	export let title: string = '';
-	export let status: string = '';
+	export let status: Status;
 	export let value: string = '';
 	export let curatorFee: string = '';
 	// optional
@@ -10,95 +14,149 @@
 	export let dateCreated: string = '';
 	export let dateOfPayout: string = '';
 	export let timeUntilPayout: string = '';
-	export let showAssignButton: boolean = true;
 	export let isExpandable: boolean = true;
 	export let onAssign: () => void = () => {};
+
+	type Status = 'created' | 'active' | 'awarded' | 'claimed' | 'sub-curator proposed';
+
+	function getStatusClass(status: string): string {
+		switch (status) {
+			case 'created':
+				return 'bg-childBountyHeaderGray';
+			case 'sub-curator proposed':
+				return 'bg-childBountyHeaderGray';
+			case 'active':
+				return 'bg-childBountyHeaderOrange';
+			case 'awarded':
+				return 'bg-childBountyHeaderGreen';
+			case 'claimed':
+				return 'bg-childBountyHeaderGreen';
+			default:
+				return 'bg-childBountyHeader';
+		}
+	}
+
+	function getButtonText(status: Status): string {
+		switch (status) {
+			case 'created':
+				return 'ASSIGN';
+			case 'sub-curator proposed':
+				return 'ACCEPT';
+			case 'active':
+				return 'AWARD';
+			case 'awarded':
+				return 'CLAIM';
+			case 'claimed':
+				return '';
+			default:
+				return '';
+		}
+	}
 </script>
 
-<div class="bg-white pb-3">
-	<!-- Child Bounty Card Header -->
-	<div class="flex justify-between items-center bg-childBountyHeader pl-2 pr-1 min-h-6">
-		<span class="text-sm text-white min-w-fit">#{id} {title}</span>
-		<span class="status bg-childBountyStatus text-white px-2 py-1 text-xs min-w-fit">{status}</span>
-		{#if isExpandable}
-			<button class="material-symbols-outlined text-white">expand_more</button>
-		{/if}
+<div class="bg-white pb-3 lg:w-[960px] xl:w-[1250px] 2xl:w-[1280px]">
+
+	<!-- Header Section -->
+	<div
+		class="flex justify-between text-white pl-6 pr-1 pt-1 min-h-6 {getStatusClass(
+			status
+		)}"
+	>
+		<div class="flex items-start">
+			<div class="flex flex-col w-60">
+				<span class="text-sm">#{id} {title}</span>
+			</div>
+			<div class="flex flex-col w-72">
+				{#if dateCreated}
+					<p class="text-xs">Created: {dateCreated}</p>
+				{/if}
+			</div>
+			<div class="flex flex-col w-[480px]">
+				{#if dateOfPayout}
+					<p class="text-xs">Date of payout: {dateOfPayout}</p>
+				{/if}
+			</div>
+		</div>
+		<div class="flex space-x-6 items-center">
+			<span class="status text-xs">{status}</span>
+			{#if isExpandable}
+				<button class="material-symbols-outlined">expand_more</button>
+			{/if}
+		</div>
 	</div>
 
 	<!-- Child Bounty Card Content -->
-	<div class="mt-3 px-3">
-		<div class="flex justify-between items-top auto-rows-auto">
-			<div class="grid gap-3">
+	<div class="flex justify-between pr-11 pt-1 pl-4">
+		<div class="mt-3 px-3 flex items-start">
+			<!-- Left column -->
+			<div class="flex flex-col w-60">
 				<section>
 					<p class="text-xs">Value</p>
-					<p class="text-sm">{value} DOT</p>
+					<p class="text-sm">{value}</p>
 				</section>
+				<section class="text-xs mt-3">
+					<p>Description</p>
+					<p>
+						This is a child bounty whose proposer address is shown in on-chain info…
+					</p>
+				</section>
+			</div>
+
+			<!-- Middle column -->
+			<div class="flex flex-col w-72">
 				<section>
 					<p class="text-xs">Sub-Curator</p>
 					<p class="sub-curator text-sm">{subCurator}</p>
 				</section>
-				{#if dateCreated}
-					<section>
-						<p class="text-xs">Date created</p>
-						<p class="text-sm">{dateCreated}</p>
-					</section>
-				{/if}
-			</div>
-
-			<div class="grid gap-2">
-				<section>
-					<p class="text-xs">Curator Fee</p>
-					<p class="text-sm">{curatorFee} DOT</p>
-				</section>
-
 				{#if beneficiary}
-					<section>
+					<section class="mt-3">
 						<p class="text-xs">Beneficiary</p>
 						<p class="beneficiary text-sm">{beneficiary}</p>
 					</section>
 				{/if}
+			</div>
 
-				{#if dateOfPayout}
-					<section>
-						<p class="text-xs">Date of payout</p>
-						<p class="text-sm">{dateOfPayout}</p>
-					</section>
-				{/if}
+			<div class="flex flex-col">
+				<section>
+					<p class="text-xs">Sub-curator Fee</p>
+					<p class="text-sm">{curatorFee} DOT</p>
+				</section>
 			</div>
 		</div>
-		{#if timeUntilPayout}
-			<section class="mt-3">
-				<p class="text-xs">Time until payout</p>
-				<p class="text-sm">{timeUntilPayout}</p>
-			</section>
-		{/if}
-	</div>
-	<div class="flex justify-between">
-		<div class="flex justify-start mt-3">
-			<button
-				class="material-symbols-outlined flex justify-center items-center text-darkgray border-darkgray border-2 rounded-full p-3 w-9 h-9 text-2xl ml-3"
-			>
-				bookmark_remove
-			</button>
-			<button
-				class="material-symbols-outlined flex justify-center items-center text-darkgray border-darkgray border-2 rounded-full p-3 w-9 h-9 text-2xl ml-3"
-			>
-				delete
-			</button>
+		<!-- Right column  -->
+		<div class="items-top">
+			<div class="flex justify-between space-x-4">
+				{#if status === 'created'}
+					<p class="mt-4">Sub-curator</p>
+				{/if}
+				<div class="grid space-y-4">
+					{#if getButtonText(status) !== ''}
+						<div class="mt-3">
+							<button
+								on:click={onAssign}
+								class="{getStatusClass(status)} text-white rounded-md font-bold py-1 min-w-32"
+							>
+								{getButtonText(status)}
+							</button>
+						</div>
+					{/if}
+					{#if status === 'created' || status === 'sub-curator proposed' || status === 'active'}
+						<button class="bg-${getStatusClass(status)} border close-button rounded-md font-bold py-1 min-w-32">
+							CLOSE
+						</button>
+					{/if}
+				</div>
+				<div class="flex gap-2 mt-3">
+					<button class="w-6 h-6"><LogoTreasuryIcon /></button>
+					<button class="w-6 h-6"><LogoPolkassembly /></button>
+					<button class="w-6 h-6"><LogoSubscan /></button>
+				</div>
+			</div>
 		</div>
-		{#if showAssignButton}
-			<button on:click={onAssign} class="bg-accent text-white rounded-md font-bold py-2 px-8 mr-3"
-				>ASSIGN</button
-			>
-		{/if}
 	</div>
 </div>
 
 <style>
-	.sub-curator,
-	.beneficiary {
-		font-family: 'PT Mono';
-	}
 	.status {
 		font-family: 'Overpass Italic';
 	}
