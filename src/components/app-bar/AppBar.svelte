27179b29eac248932b362f16f9ffe5b7da6a09ae
BlertaGalani <98878525+BlertaGalani@users.noreply.github.com>
2024-10-16 17:42:09 +0200

    update app bar, fix create bounty issues, minor design changes

--- src/components/app-bar/AppBar.svelte
+++ src/components/app-bar/AppBar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { isLoggedIn, loggedAccounts, activeAccount } from '../../stores';
+	import LogoBountyManagerMobile from '../svg/header-footer-logos/LogoBountyManagerMobile.svelte';
 	import LoginDialog from './LoginDialog.svelte';
 
 	let loginDialogOpen = false;
@@ -9,21 +10,30 @@
 	}
 </script>
 
-<div class="flex justify-end h-20 bg-primary">
-	{#if !$isLoggedIn}
-		<button class=" text-white basic-button" on:click={() => showLoginDialog()}> signin</button>
+<header class="relative flex items-center justify-between h-20 bg-primary px-4 sm:px-12">
+	<div class="md:absolute md:left-1/2 md:transform md:-translate-x-1/2">
+		<LogoBountyManagerMobile/>
+	</div>
+	<div class="ml-auto">
+		{#if !$isLoggedIn}
+			<button class=" text-white" on:click={() => showLoginDialog()}> sign in</button>
 
-		<w3m-button></w3m-button>
-	{:else}
-		<select class="w-52 h-10 rounded-md" bind:value={$activeAccount}>
-			{#each $loggedAccounts as account}
-				<option value={account}>
-					({account.meta.name || ''}) {account.address.toWellFormed().substring(0, 15) + '...'}
-				</option>
-			{/each}
-		</select>
-	{/if}
-</div>
+			<w3m-button></w3m-button>
+		{:else}
+			<select class="w-52 h-10 rounded-md" bind:value={$activeAccount}>
+				{#each $loggedAccounts as account}
+					<option value={account}>
+						({account.meta.name || ''}) {account.address.toWellFormed().substring(0, 15) + '...'}
+					</option>
+				{/each}
+			</select>
+		{/if}
+	</div>
+</header>
+
+<section class="h-10 bg-secondary w-full">
+
+</section>
 
 {#if loginDialogOpen}
 	<LoginDialog title="CHOOSE YOUR WALLET" bind:open={loginDialogOpen} />
