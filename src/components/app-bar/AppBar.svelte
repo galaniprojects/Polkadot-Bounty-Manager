0e0eee43f21cf98d7768fe61290d1afe1263953d
BlertaGalani <98878525+BlertaGalani@users.noreply.github.com>
2024-10-17 15:38:03 +0200

    update logos

--- src/components/app-bar/AppBar.svelte
+++ src/components/app-bar/AppBar.svelte
@@ -23,13 +23,13 @@
 </script>
 
 <header class="relative flex items-center justify-between h-20 bg-primary px-4 sm:px-12">
-	<div class="md:absolute md:left-1/2 md:transform md:-translate-x-1/2">
-		<span class="hidden lg:inline-flex"><LogoBountyManagerDesktop /></span>
-		<span class="lg:hidden"><LogoBountyManagerMobile /></span>
+	<div class="md:absolute md:left-1/2 md:transform md:-translate-x-1/2 pt-2">
+		<span class="hidden md:inline-flex"><LogoBountyManagerDesktop /></span>
+		<span class="md:hidden"><LogoBountyManagerMobile /></span>
 	</div>
 	<div class="ml-auto">
 		{#if !$isLoggedIn}
-			<button class=" text-white" on:click={() => showLoginDialog()}> sign in</button>
+			<button class=" text-white" on:click={() => showLoginDialog()}>sign in</button>
 
 			<w3m-button></w3m-button>
 		{:else}
