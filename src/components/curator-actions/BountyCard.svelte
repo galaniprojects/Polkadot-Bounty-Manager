8151f54a4c91d8c1bc6678e406ccda827ec9bf3e
BlertaGalani <98878525+BlertaGalani@users.noreply.github.com>
2024-10-18 14:24:50 +0200

    add collapse/expand functionality to parent bounties

--- src/components/curator-actions/BountyCard.svelte
+++ src/components/curator-actions/BountyCard.svelte
@@ -11,11 +11,15 @@
 	import LogoSubscanWhite from '../svg/curator-actions-logo/LogoSubscanWhite.svelte';
 	import LogoSubsquareWhite from '../svg/curator-actions-logo/LogoSubsquareWhite.svelte';
 	import BountyCardHeader from './BountyCardHeader.svelte';
+	import { bounties } from '../../stores';
 
 	export let bounty: Bounty;
+
 	let acceptCuratorRuleDialogOpen = false;
 	let awardBountyDialogOpen = false;
-
+	let curatorsExpended = false;
+	let detailsExpended = false;
+	let expandedBounties: Record<number, boolean> = {};
 	let status: 'proposed' | 'approved' | 'funded' | 'curator proposed' | 'active' | 'pending payout';
 
 	onMount(() => {
@@ -47,9 +51,6 @@
 		}
 	});
 
-	let curatorsExpended = false;
-	let detailsExpended = false;
-
 	function handleCuratorsToggleClick() {
 		curatorsExpended = !curatorsExpended;
 	}
@@ -57,206 +58,240 @@
 	function handleMoreDetailsToggleClick() {
 		detailsExpended = !detailsExpended;
 	}
+
+	onMount(() => {
+		expandedBounties[0] = true;
+	});
+
+	function handleExpandBountyToggleClick(index: number) {
+		expandedBounties[index] = !expandedBounties[index];
+	}
 </script>
 
-<div class="bg-curatorMainBackground overflow-x-hidden rounded-md my-6">
-	<BountyCardHeader {bounty} {status}></BountyCardHeader>
+{#each $bounties as bounty, index}
+	<div class="bg-curatorMainBackground overflow-hidden rounded-md my-6">
+		<BountyCardHeader
+			{bounty}
+			bountyIndex={index}
+			{status}
+			isParentExpanded={expandedBounties[index] || false}
+			on:toggleExpand={() => handleExpandBountyToggleClick(index)}
+		></BountyCardHeader>
 
-	<!-- Content Section -->
-	<div class="bg-curatorCarousel xl:pt-6 text-white w-full p-0 sm:p-3">
-		<!-- Desktop design -->
-		<div class="hidden lg:flex lg:flex-col gap-3 justify-start lg:ml-7">
-			<section class="flex flex-col lg:flex lg:flex-row lg:justify-between">
-				<section class="flex flex-col lg:flex-row">
-					<div class="lg:w-[250px] xl:w-[490px]">
+		{#if expandedBounties[index]}
+			<!-- Content Section -->
+			<div class="bg-curatorCarousel xl:pt-6 text-white w-full p-0 sm:p-3">
+				<!-- Desktop design -->
+				<div class="hidden lg:flex lg:flex-col gap-3 justify-start lg:ml-7">
+					<section class="flex flex-col lg:flex lg:flex-row lg:justify-between">
+						<section class="flex flex-col lg:flex-row">
+							<div class="lg:w-[250px] xl:w-[490px]">
+								<p class="text-xs">Remaining Balance</p>
+								<p class="text-2xl"><span>{convertPlanckToDot(bounty.value)}</span> DOT</p>
+							</div>
+
+							<div class="mt-4 lg:mt-0">
+								<p class="text-xs">Curator Fee</p>
+								<p class="text-md"><span>{convertPlanckToDot(bounty.fee)}</span> DOT</p>
+							</div>
+						</section>
+						<div
+							class="bounty-button-container lg:flex lg:justify-end lg:space-x-3 lg:mr-12 2xl:mr-44"
+						>
+							<p class="hidden text-center lg:inline-flex">Curators</p>
+							<div class="space-x-2">
+								<button
+									on:click={handleCuratorsToggleClick}
+									class="w-full button-popup font-bold rounded-md pt-1 lg:min-w-32 lg:h-fit"
+								>
+									SHOW <span class="lg:hidden">CURATORS</span>
+								</button>
+							</div>
+						</div>
+					</section>
+					<section class="flex-col lg:flex lg:flex-row lg:justify-between">
+						<section class="flex justify-start">
+							<div class="text-xs lg:w-[250px] xl:w-[490px] pr-3">
+								<p>Description</p>
+								<p>
+									This proposal stems from the need to enhance system upgrade testing procedures,
+									minimizing feature regression. Recent discussions in various channels, spurred by
+									…
+								</p>
+							</div>
+							<div class="flex justify-between lg:space-x-8 xl:space-x-16">
+								<section>
+									<p class="text-xs">Created</p>
+									<p>8th Aug 2024</p>
+								</section>
+
+								<section class="flex-col text-start">
+									<p class="text-xs">Expiration date</p>
+									<p>27th Sep 2024</p>
+								</section>
+							</div>
+						</section>
+						<div class="flex justify-center space-x-2.5 lg:mr-12 2xl:mr-44">
+							<button class="w-10 h-10 lg:w-6 lg:h-6">
+								<LogoTreasuryWhite />
+							</button>
+							<button class="w-10 h-10 lg:w-6 lg:h-6">
+								<LogoPolkassemblyWhite />
+							</button>
+							<button class="w-10 h-10 lg:w-6 lg:h-6">
+								<LogoSubscanWhite />
+							</button>
+							<button class="w-10 h-10 lg:w-6 lg:h-6">
+								<LogoSubsquareWhite />
+							</button>
+						</div>
+					</section>
+				</div>
+			</div>
+
+			<!-- Mobile design -->
+			<div class="lg:hidden">
+				<div class="grid space-y-5 bg-curatorCarousel px-[10px] py-5 text-white">
+					<button
+						on:click={handleCuratorsToggleClick}
+						class="w-full lg:w-fit h-12 button-popup font-bold rounded-md"
+						>SHOW CURATORS
+					</button>
+					<div>
 						<p class="text-xs">Remaining Balance</p>
 						<p class="text-2xl"><span>{convertPlanckToDot(bounty.value)}</span> DOT</p>
 					</div>
+				</div>
+				<div class="flex justify-center items-center text-white">
+					{#if !detailsExpended}
+						<div class="bg-curatorCarousel max-w-fit rounded-b-md max-h-[24px]">
+							<button class="text-xs align-top mt-1 ml-2" on:click={handleMoreDetailsToggleClick}>
+								more details
+							</button>
+							<button
+								class="material-symbols-outlined align-top w-6 h-6"
+								on:click={handleMoreDetailsToggleClick}
+							>
+								keyboard_arrow_down
+							</button>
+						</div>
+					{/if}
+				</div>
 
-					<div class="mt-4 lg:mt-0">
-						<p class="text-xs">Curator Fee</p>
-						<p class="text-md"><span>{convertPlanckToDot(bounty.fee)}</span> DOT</p>
-					</div>
-				</section>
-				<div class="bounty-button-container lg:flex lg:justify-end lg:space-x-3 lg:mr-12 2xl:mr-44">
-					<p class="hidden text-center lg:inline-flex">Curators</p>
-					<div class="space-x-2">
-						<button
-							on:click={handleCuratorsToggleClick}
-							class="w-full button-popup font-bold rounded-md pt-1 lg:min-w-32 lg:h-fit"
-						>
-							SHOW <span class="lg:hidden">CURATORS</span>
-						</button>
-					</div>
-				</div>
-			</section>
-			<section class="flex-col lg:flex lg:flex-row lg:justify-between">
-				<section class="flex justify-start">
-					<div class="text-xs lg:w-[250px] xl:w-[490px] pr-3">
-						<p>Description</p>
-						<p>
-							This proposal stems from the need to enhance system upgrade testing procedures,
-							minimizing feature regression. Recent discussions in various channels, spurred by …
-						</p>
-					</div>
-					<div class="flex justify-between lg:space-x-8 xl:space-x-16">
-						<section>
-							<p class="text-xs">Created</p>
-							<p>8th Aug 2024</p>
-						</section>
-
-						<section class="flex-col text-start">
-							<p class="text-xs">Expiration date</p>
-							<p>27th Sep 2024</p>
-						</section>
-					</div>
-				</section>
-				<div class="flex justify-center space-x-2.5 lg:mr-12 2xl:mr-44">
-					<button class="w-10 h-10 lg:w-6 lg:h-6">
-						<LogoTreasuryWhite />
-					</button>
-					<button class="w-10 h-10 lg:w-6 lg:h-6">
-						<LogoPolkassemblyWhite />
-					</button>
-					<button class="w-10 h-10 lg:w-6 lg:h-6">
-						<LogoSubscanWhite />
-					</button>
-					<button class="w-10 h-10 lg:w-6 lg:h-6">
-						<LogoSubsquareWhite />
-					</button>
-				</div>
-			</section>
-		</div>
-	</div>
-
-	<!-- Mobile design -->
-	<div class="lg:hidden">
-		<div class="grid space-y-5 bg-curatorCarousel px-[10px] py-5 text-white">
-			<button
-				on:click={handleCuratorsToggleClick}
-				class="w-full lg:w-fit h-12 button-popup font-bold rounded-md"
-				>SHOW CURATORS
-			</button>
-			<div>
-				<p class="text-xs">Remaining Balance</p>
-				<p class="text-2xl"><span>{convertPlanckToDot(bounty.value)}</span> DOT</p>
-			</div>
-		</div>
-		<div class="flex justify-center items-center text-white">
-			{#if !detailsExpended}
-				<div class="bg-curatorCarousel max-w-fit rounded-b-md max-h-[24px]">
-					<button class="text-xs align-top mt-1 ml-2" on:click={handleMoreDetailsToggleClick}>
-						more details
-					</button>
-					<button
-						class="material-symbols-outlined align-top w-6 h-6"
-						on:click={handleMoreDetailsToggleClick}
-					>
-						keyboard_arrow_down
-					</button>
-				</div>
-			{/if}
-		</div>
-
-		{#if detailsExpended}
-			<div class="bg-curatorCarousel text-white px-[10px] pb-6 space-y-5">
-				<div class="space-y-1">
-					<p class="text-xs">Curator Fee</p>
-					<p class="text-md"><span>{convertPlanckToDot(bounty.fee)}</span> DOT</p>
-				</div>
-				<section class="text-xs space-y-1">
-					<p>Description</p>
-					<p>
-						This proposal stems from the need to enhance system upgrade testing procedures,
-						minimizing feature regression. Recent discussions in various channels, spurred by …
-					</p>
-				</section>
-				<div class="flex justify-between">
-					<section>
-						<p class="text-xs">Created</p>
-						<p>8th Aug 2024</p>
-					</section>
-
-					<section class="flex-col text-end">
-						<p class="text-xs">Expiration date</p>
-						<p>27th Sep 2024</p>
-					</section>
-				</div>
-				<div class="flex justify-center items-center space-x-5">
-					<button class="w-10 h-10 lg:w-6 lg:h-6">
-						<LogoTreasuryWhite />
-					</button>
-					<button class="w-10 h-10 lg:w-6 lg:h-6">
-						<LogoPolkassemblyWhite />
-					</button>
-					<button class="w-10 h-10 lg:w-6 lg:h-6">
-						<LogoSubscanWhite />
-					</button>
-					<button class="w-10 h-10 lg:w-6 lg:h-6">
-						<LogoSubsquareWhite />
-					</button>
-				</div>
-			</div>
-			<div class="flex justify-center items-center">
 				{#if detailsExpended}
-					<div class="text-white bg-curatorCarousel max-w-fit rounded-b-md max-h-[24px]">
-						<button class="text-xs align-top mt-1 ml-2" on:click={handleMoreDetailsToggleClick}>
-							less details
-						</button>
-						<button
-							class="material-symbols-outlined align-top w-6 h-6"
-							on:click={handleMoreDetailsToggleClick}
-						>
-							keyboard_arrow_up
-						</button>
+					<div class="bg-curatorCarousel text-white px-[10px] pb-6 space-y-5">
+						<div class="space-y-1">
+							<p class="text-xs">Curator Fee</p>
+							<p class="text-md"><span>{convertPlanckToDot(bounty.fee)}</span> DOT</p>
+						</div>
+						<section class="text-xs space-y-1">
+							<p>Description</p>
+							<p>
+								This proposal stems from the need to enhance system upgrade testing procedures,
+								minimizing feature regression. Recent discussions in various channels, spurred by …
+							</p>
+						</section>
+						<div class="flex justify-between">
+							<section>
+								<p class="text-xs">Created</p>
+								<p>8th Aug 2024</p>
+							</section>
+
+							<section class="flex-col text-end">
+								<p class="text-xs">Expiration date</p>
+								<p>27th Sep 2024</p>
+							</section>
+						</div>
+						<div class="flex justify-center items-center space-x-5">
+							<button class="w-10 h-10 lg:w-6 lg:h-6">
+								<LogoTreasuryWhite />
+							</button>
+							<button class="w-10 h-10 lg:w-6 lg:h-6">
+								<LogoPolkassemblyWhite />
+							</button>
+							<button class="w-10 h-10 lg:w-6 lg:h-6">
+								<LogoSubscanWhite />
+							</button>
+							<button class="w-10 h-10 lg:w-6 lg:h-6">
+								<LogoSubsquareWhite />
+							</button>
+						</div>
+					</div>
+					<div class="flex justify-center items-center">
+						{#if detailsExpended}
+							<div class="text-white bg-curatorCarousel max-w-fit rounded-b-md max-h-[24px]">
+								<button class="text-xs align-top mt-1 ml-2" on:click={handleMoreDetailsToggleClick}>
+									less details
+								</button>
+								<button
+									class="material-symbols-outlined align-top w-6 h-6"
+									on:click={handleMoreDetailsToggleClick}
+								>
+									keyboard_arrow_up
+								</button>
+							</div>
+						{/if}
 					</div>
 				{/if}
 			</div>
-		{/if}
-	</div>
 
-	<section class="flex flex-col space-y-1 px-3 py-5 lg:justify-end lg:mr-12 lg:space-y-3 2xl:pr-36">
-		<div class="flex flex-col space-y-1 lg:flex-row lg:space-x-3 lg:justify-end">
-			<p class="pt-2 text-sm text-white">
-				<span class="lg:hidden">Add</span> Beneficiary Claim Form
-			</p>
-			<button
-				class="w-full h-12 button-popup font-bold rounded-md lg:w-fit lg:h-auto lg:pt-1 lg:min-w-32"
-				><span class="lg:hidden">BENEFICIARY CLAIM FORM</span>
-				<span class="hidden lg:inline-flex">ADD</span></button
+			<section
+				class="flex flex-col space-y-1 px-3 py-5 lg:justify-end lg:mr-12 lg:space-y-3 2xl:pr-36"
 			>
-		</div>
+				<div class="flex flex-col space-y-1 lg:flex-row lg:space-x-3 lg:justify-end">
+					<p class="pt-2 text-sm text-white">
+						<span class="lg:hidden">Add</span> Beneficiary Claim Form
+					</p>
+					<button
+						class="w-full h-12 button-popup font-bold rounded-md lg:w-fit lg:h-auto lg:pt-1 lg:min-w-32"
+						><span class="lg:hidden">BENEFICIARY CLAIM FORM</span>
+						<span class="hidden lg:inline-flex">ADD</span></button
+					>
+				</div>
 
-		{#if status === 'proposed' || status === 'approved' || status === 'funded'}
-			<div class="flex flex-col space-y-1 lg:flex-row lg:space-x-3 lg:justify-end">
-				<p class="pt-2 text-sm text-white">Curator Role</p>
+				{#if status === 'proposed' || status === 'approved' || status === 'funded'}
+					<div class="flex flex-col space-y-1 lg:flex-row lg:space-x-3 lg:justify-end">
+						<p class="pt-2 text-sm text-white">Curator Role</p>
 
+						<button
+							class="w-full h-12 button-popup font-bold rounded-md lg:w-fit lg:h-auto lg:pt-1 lg:max-w-32 lg:px-7"
+							on:click={() => {
+								goto('/');
+							}}>PROPOSE</button
+						>
+					</div>
+				{/if}
+
+				<div class="flex flex-col space-y-1 lg:flex-row lg:space-x-3 lg:justify-end">
+					<p class="pt-2 text-sm text-white">Curator Role</p>
+					<button
+						class="w-full h-12 button-popup font-bold rounded-md lg:w-fit lg:h-auto lg:pt-1 lg:max-w-32 lg:px-9"
+						on:click={() => {
+							acceptCuratorRuleDialogOpen = true;
+						}}>ACCEPT</button
+					>
+				</div>
+			</section>
+
+			<div class="w-full pr-6">
+				{#if status === 'active'}
+					<ChildBountiesSection {bounty} childBounties={bounty.childBounties} />
+				{/if}
+			</div>
+
+			<!-- Footer -->
+			<div class="flex justify-end px-5 my-4 lg:px-10">
 				<button
-					class="w-full h-12 button-popup font-bold rounded-md lg:w-fit lg:h-auto lg:pt-1 lg:max-w-32 lg:px-7"
-					on:click={() => {
-						goto('/');
-					}}>PROPOSE</button
+					class="flex items-center pt-5 pb-1"
+					on:click={() => handleExpandBountyToggleClick(index)}
 				>
+					<p class="text-white text-xs">close bounty view</p>
+					<span class="material-symbols-outlined text-white text-3xl"> keyboard_arrow_up </span>
+				</button>
 			</div>
 		{/if}
-
-		<div class="flex flex-col space-y-1 lg:flex-row lg:space-x-3 lg:justify-end">
-			<p class="pt-2 text-sm text-white">Curator Role</p>
-			<button
-				class="w-full h-12 button-popup font-bold rounded-md lg:w-fit lg:h-auto lg:pt-1 lg:max-w-32 lg:px-9"
-				on:click={() => {
-					acceptCuratorRuleDialogOpen = true;
-				}}>ACCEPT</button
-			>
-		</div>
-	</section>
-
-	<div class="w-full pr-6">
-		{#if status === 'active'}
-			<ChildBountiesSection {bounty} childBounties={bounty.childBounties} />
-		{/if}
 	</div>
-</div>
+{/each}
 {#if acceptCuratorRuleDialogOpen}
 	<AcceptCuratorRule bind:open={acceptCuratorRuleDialogOpen} {bounty} />
 {/if}
--- src/components/curator-actions/BountyCardHeader.svelte
+++ src/components/curator-actions/BountyCardHeader.svelte
@@ -1,17 +1,34 @@
 <script lang="ts">
 	import type { Bounty } from '../../types/bounty';
 	import { truncateString } from '../../utils/common';
+	import { createEventDispatcher } from 'svelte';
 
 	export let bounty: Bounty;
 	export let status: string;
+	export let isParentExpanded: boolean;
+	export let bountyIndex: number;
+	const dispatch = createEventDispatcher();
+
+	function toggleExpand() {
+		dispatch('toggleExpand');
+	}
 </script>
 
-<section class="text-white max-h-30 px-5 my-4 lg:px-10">
-	<div class="flex flex-col lg:flex-row justify-between items-start lg:items-center">
-		<span class="text-xl lg:text-2xl">
-			#{`${bounty.id}`}
-			{`${bounty.description}`}
-		</span>
+<section class="text-white max-h-30 px-5 my-4 lg:px-10" on:click={toggleExpand}>
+	<div class="flex justify-between">
+		<div class="flex flex-col lg:flex-row justify-between items-start lg:items-center">
+			<span class="text-xl lg:text-2xl">
+				#{`${bounty.id}`}
+				{`${bounty.description}`}
+			</span>
+		</div>
+		<button class="material-symbols-outlined text-white text-3xl">
+			{#if isParentExpanded}
+				keyboard_arrow_up
+			{:else}
+				keyboard_arrow_down
+			{/if}
+		</button>
 	</div>
 
 	<div class="flex flex-col lg:flex-row lg:justify-start text-xs lg:mt-0">
