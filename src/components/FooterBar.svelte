68ab1a8ac50872a3a8f021a43a760ff1a41f68d9
BlertaGalani <98878525+BlertaGalani@users.noreply.github.com>
2024-10-21 11:59:21 +0200

    add footer desktop

---
+++ src/components/FooterBar.svelte
@@ -0,0 +1,13 @@
+<script>
+	import LogoBountyManagerDesktop from './svg/header-footer-logos/LogoBountyManagerDesktop.svelte';
+	import LogoBountyManagerMobile from './svg/header-footer-logos/LogoBountyManagerMobile.svelte';
+</script>
+
+<footer class="bg-primary relative flex justify-between items-center px-3 sm:px-12 py-6">
+	<button class="text-sm text-white flex-grow text-left">Imprint</button>
+	<div class="absolute inset-0 flex justify-center items-center">
+		<span class="hidden lg:inline-flex"><LogoBountyManagerDesktop /></span>
+		<span class="lg:hidden"><LogoBountyManagerMobile /></span>
+	</div>
+	<p class="text-sm text-white flex-grow text-right">galaniprojects GMbH 2024 | all rights reserved</p>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,11 +4,13 @@
 	import AppBar from '../components/app-bar/AppBar.svelte';
 	import LoadingScreen from '../components/LoadingScreen.svelte';
 	import { loadingDialogState } from '../stores';
+	import FooterBar from '../components/FooterBar.svelte';
 </script>
 
 <AppBar />
 <LoadingScreen bind:dialogState={$loadingDialogState} />
 <slot />
+<FooterBar />
 
 <style lang="postcss">
 	:global(html) {
