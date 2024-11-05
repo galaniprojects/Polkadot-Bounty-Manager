7ff5510515a08b93499f091a2e8ad2efc3f8f182
BlertaGalani <98878525+BlertaGalani@users.noreply.github.com>
2024-11-05 17:24:15 +0100

    add T&C and back button

--- src/routes/imprint/+page.svelte
+++ src/routes/imprint/+page.svelte
@@ -2,8 +2,8 @@
 	const email = 'info@galaniprojects.de';
 </script>
 
-<div class="bg-curatorMainBackground rounded-lg my-5 mx-12 px-3 py-5 flex-col space-y-5 text-white">
-	<h1 class="text-xl">Imprint</h1>
+<div class="bg-curatorMainBackground rounded-lg my-5 mx-12 px-5 py-5 flex-col space-y-5 text-white">
+	<h2 class="text-xl">Imprint</h2>
 	<p>
 		INFORMATION IN ACCORDANCE WITH SECTION 5 <br />
 		OF THE GERMAN TELEMEDIENGESETZ (TMG)
@@ -42,6 +42,133 @@
 		<a href={`mailto:${email}`} target="_self" class="underline underline-offset-2"
 			>info@galaniprojects.de</a
 		>
+	</p>
+
+	<h2 class="text-xl">Terms and Conditions for Bounty Manager</h2>
+
+	<h3 class="text-xl">Application of These Terms and Conditions</h3>
+
+	<p>
+		For the usage of the Bounty Manager, these Terms and Conditions shall apply. The Services are
+		provided to you via software (the “Software” or “Bounty Manager”) created and run by
+		galaniprojects GmbH (hereinafter referred to as “galaniprojects”, “us”, “we” or “our”) as
+		defined below. The software is published and you are free to check out the code for the Bounty
+		Manager under: <br /> [https://github.com/galaniprojects/Polkadot-Bounty-Manager]
+	</p>
+	<p>
+		PLEASE READ THE TERMS AND CONDITIONS CAREFULLY TO ENSURE THAT YOU UNDERSTAND EACH PROVISION. IF
+		YOU DO NOT AGREE TO ALL OF THE TERMS, DO NOT ACCESS OR USE THE SOFTWARE. <br /> YOU ACCEPT THE
+		TERMS AND CONDITIONS, EITHER BY CLICKING TO SIGNIFY ACCEPTANCE, OR BY TAKING ANY ONE OR MORE OF
+		THE FOLLOWING ACTIONS: ACCESSING OR USING THE APPLICABLE SOFTWARE, YOU AGREE TO BE BOUND BY THE
+		TERMS AND CONDITIONS. <br /> YOU REPRESENT AND WARRANT THAT YOU ARE 18 YEARS OLD OR OLDER AND HAVE
+		THE RIGHT AND AUTHORITY TO ENTER AND COMPLY WITH THE TERMS OF USE.
+	</p>
+
+	<h3 class="text-xl">What the Bounty Manager Is</h3>
+	<p>
+		The Bounty Manager allows users to manage and execute bounties using various blockchain
+		accounts. Therefore, with the full version you can:
+	</p>
+	<ul class="list-disc m-10">
+		<li>
+			Sign In using wallets such as Polkadot.js Extension, Nova Wallet, Talisman, or WalletConnect
+			with Multix.
+		</li>
+		<li>
+			Bounty Setup: Create on-chain bounties, initiate approval and curator proposal referendums,
+			and have descriptions posted on social platforms via API.
+		</li>
+		<li>
+			Curator Actions: Log in to accept roles, manage bounty funds, extend the bounty expiration,
+			add child bounties, write prespecified remarks including a URL and assign roles.
+		</li>
+		<li>Sub-curator Actions: Accept roles and manage child bounties.</li>
+		<li>
+			Externally Sourced Descriptions: See user written description of bounties and child bounties,
+			sourced through an API from either the Polkassembly or Subsquare platform.
+		</li>
+		<li>
+			External Links: Follow external links to platforms such as Polkassembly, Dotreasury and
+			Subsquare regarding the selected bounty or child bounty. Follow a link to the URL written with
+			a prespecified remark from the Curators.
+		</li>
+	</ul>
+	<p>
+		The data generated in and for your bounty activities is stored according to the blockchain
+		protocols and platform standards. It is critical to ensure the security of your login
+		credentials and wallet information.
+	</p>
+	<h3 class="text-xl">Liability</h3>
+	<p>
+		galaniprojects GmbH (hereinafter referred to as "galaniprojects") is liable for damages that are
+		based on an intentional or grossly negligent breach of duty by galaniprojects, its legal
+		representatives, or various agents. <br /> In the event of a breach of essential contractual
+		duties, galaniprojects shall only be liable for the contractually typical, foreseeable damage if
+		this was simply caused by negligence. Significant contractual obligations are those whose
+		fulfillment enables the proper execution of the contract in the first place and whose compliance
+		you can regularly rely on. <br />The limitation of the two preceding paragraphs also applies to
+		the legal representatives and various agents of galaniprojects if claims are asserted directly
+		against them. The liability limitations resulting from the two preceding paragraphs do not apply
+		insofar as galaniprojects fraudulently concealed the defect or assumed a guarantee for the
+		quality of the Bounty Manager. <br /> Liability for culpable injury to life, limb, and health
+		and liability under Product Liability Act remain unaffected. <br /> Any additional claims for damages
+		are excluded.
+	</p>
+
+	<h3>Risk Information</h3>
+	<p>
+		The following risk information contains a list of risks associated with the use of the Bounty
+		Manager. The list is not exhaustive. It is not excluded that further unknown or unpredictable
+		risks exist.
+	</p>
+	<ul class="list-disc m-10">
+		<li>
+			Loss of access credentials will result in a complete loss of accessibility of the Bounty
+			Manager in question.
+		</li>
+		<li>
+			The Bounty Manager relies on the functionality of the wallets used to log into the Bounty
+			Manager. Errors or dysfunctions in the underlying software of the wallets or its
+			functionalites, may adversely affect the Bounty Manager.
+		</li>
+		<li>
+			The Bounty Manager relies on the functionality of the Polkadot blockchain. Errors or
+			dysfunctions, including failure of the blockchain or its ecosystem, may adversely affect the
+			Bounty Manager.
+		</li>
+		<li>
+			The Bounty Manager, particularly its underlying software, may be subject to hacking or other
+			malicious interference by unauthorized third parties, resulting in the loss, theft, or other
+			violation of data.
+		</li>
+		<li>
+			Externally Sourced Descriptions: See user written description of bounties and child bounties,
+			sourced through an API from either the Polkassembly or Subsquare platform.
+		</li>
+		<li>
+			Communication through internet-based systems is susceptible to data/information being read out
+			and possibly altered.
+		</li>
+	</ul>
+	<h3>Right to Change the Software and These Terms and Conditions</h3>
+	<p>
+		galaniprojects reserves the right to change the Software as well as the commercial and
+		non-commercial conditions for its usage. <br /> galaniprojects also reserves the right to change
+		these Terms and Conditions at any time for any future products at our sole discretion.
+		<br />Such changes will be made via uploading new Terms and Conditions, the Software, and/or
+		other information to the Website. Usage of the Bounty Manager downloaded after this will fall
+		under these new Terms and Conditions and will be handled by the new version of the Software.
+	</p>
+	<h3>Miscellaneous</h3>
+	<p>
+		These Terms and Conditions and the entire legal relationship between the parties shall be
+		governed by the laws of the Federal Republic of Germany, excluding the UN Convention on
+		Contracts for the International Sale of Goods (CISG), unless the choice of law is legally
+		prohibited. <br /> If a term of this agreement is invalid or unenforceable, the remaining
+		provisions will continue in full force and effect. <br /> The place of performance and exclusive
+		place of jurisdiction for all disputes arising from these Terms and Conditions and the entire legal
+		relationship between the parties shall be galaniprojects' registered office, unless choice of jurisdiction
+		is legally prohibited.
 	</p>
 </div>
 
