1f57a4c260337f214e8bb34e2546629c3d0c250a
Abdulrahim Al Methiab <31316147+abdulmth@users.noreply.github.com>
2024-11-06 14:14:06 +0100

    Adjust beta version note (#324)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <div class="bg-accent text-xs md:text-lg text-white flex justify-center items-center">
-	<p class="m-2">This is a beta version, some features may not function properly yet.</p>
+	<p class="m-2">This is a beta version, some features may still be incomplete.</p>
 </div>
 <AppBar />
 <LoadingScreen bind:dialogState={$loadingDialogState} />
