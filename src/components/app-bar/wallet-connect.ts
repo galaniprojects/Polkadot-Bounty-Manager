68b9071e72e3210bde167bcde4201d50ed48fc30
Abdulrahim Al Methiab <abdul@kilt.io>
2024-11-21 16:59:01 +0100

    Acticate polkadot chain only for WC

--- src/components/app-bar/wallet-connect.ts
+++ src/components/app-bar/wallet-connect.ts
@@ -40,8 +40,8 @@ export function createWCConnection() {
 			}
 		},
 		chainIds: [
-			// 'polkadot:91b171bb158e2d3848fa23a9f1c25182', //polkadot
-			'polkadot:77afd6190f1554ad45fd0d31aee62aac' // Paseo
+			'polkadot:91b171bb158e2d3848fa23a9f1c25182' //polkadot
+			// 'polkadot:77afd6190f1554ad45fd0d31aee62aac' // Paseo
 		]
 	});
 }
