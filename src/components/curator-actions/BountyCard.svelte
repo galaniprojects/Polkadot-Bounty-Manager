78fbd41d0023ba3a8e2fd5bb7343d5406a249dc6
Abdulrahim Al Methiab <abdul@kilt.io>
2024-11-26 14:05:36 +0100

    revert removing award bounty

--- src/components/curator-actions/BountyCard.svelte
+++ src/components/curator-actions/BountyCard.svelte
@@ -4,15 +4,17 @@
 	import BountyOperations from './BountyOperations.svelte';
 	import ChildBountiesSection from './child-bounties/ChildBountiesSection.svelte';
 	import { parse } from 'marked';
-	import { dotApi } from '../../stores';
+	import { activeAccount, dotApi, showAllCuratorOptions } from '../../stores';
 	import BountyCardDetails from './BountyCardDetails.svelte';
 	import { formatPlanckToDot } from '../../utils/polkadot';
+	import AwardBounty from './operations/AwardBounty.svelte';
 
 	export let bounty: Bounty;
 	export let expanded: boolean;
 
 	let description: string | undefined;
 	let remainingBalance: string | undefined;
+	let awardBountyDialogOpen = false;
 
 	// Handle description and balance fetch
 	$: if (expanded) {
@@ -70,6 +72,22 @@
 			{/if}
 		</div>
 
+		<div
+			class="flex flex-col space-y-1 px-3 pt-0 lg:pt-3 lg:justify-end lg:mr-12 lg:space-y-3 2xl:pr-36"
+		>
+			{#if $showAllCuratorOptions || (bounty.status === BountyStatus.Active && bounty.childBounties.length === 0 && $activeAccount && bounty.curator === $activeAccount.address)}
+				<div class="flex flex-col space-y-1 lg:flex-row lg:space-x-3 lg:justify-end">
+					<p class="pt-2 text-sm text-white">Award bounty</p>
+					<button
+						class="w-full h-12 button-popup font-bold rounded-md lg:w-fit lg:h-auto lg:pt-1 lg:max-w-32 lg:px-5"
+						on:click={() => {
+							awardBountyDialogOpen = true;
+						}}>READ FIRST</button
+					>
+				</div>
+			{/if}
+		</div>
+
 		<!-- Footer -->
 		<div class="flex justify-end px-5 my-4 lg:px-10">
 			<button class="flex items-center pt-5 pb-1" on:click={toggleExpanded}>
@@ -79,3 +97,6 @@
 		</div>
 	{/if}
 </div>
+{#if awardBountyDialogOpen}
+	<AwardBounty bind:open={awardBountyDialogOpen} {bounty} />
+{/if}
