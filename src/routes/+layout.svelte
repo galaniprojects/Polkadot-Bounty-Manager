b9e99aac7786cd87a49a4fa37306b081d9c570ef
Tom Adler <me@arty.name>
2024-12-09 12:04:05 +0100

    environment variables

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { PUBLIC_HIDE_TEST_BAR } from '$env/static/public';
 	import '../app.css';
 	import AppBar from '../components/app-bar/AppBar.svelte';
 	import LoadingScreen from '../components/LoadingScreen.svelte';
@@ -7,7 +8,9 @@
 	import TestBar from '../components/TestBar.svelte';
 </script>
 
-<TestBar />
+{#if !PUBLIC_HIDE_TEST_BAR}
+	<TestBar />
+{/if}
 <div class="bg-accent text-xs md:text-lg text-white flex justify-center items-center">
 	<p class="m-2">This is a beta version, some features may still be incomplete.</p>
 </div>
