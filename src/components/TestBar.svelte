6a626914515230220a835cc72d97051e7b79867b
Tom Adler <me@arty.name>
2024-12-10 15:01:26 +0100

    removed endpoint selector

--- src/components/TestBar.svelte
+++ src/components/TestBar.svelte
@@ -11,24 +11,9 @@
 			dotApi.set(createTypedApi(node));
 		}
 	});
-
-	function onChange(this: HTMLSelectElement) {
-		sessionStorage.setItem('node', this.value);
-		window.location.reload();
-	}
 </script>
 
 <div class="w-100">
-	<select
-		class="border full rounded-md h-7 px-1 pt-1"
-		bind:value={$nodeEndpoint}
-		on:change={onChange}
-		name="spenders"
-		id="spenders"
-	>
-		<option value="wss://chopsticks.rust-2.kilt.io">wss://chopsticks.rust-2.kilt.io</option>
-		<option value="ws://localhost:8000">ws://localhost:8000</option>
-	</select>
 	<div class="mt-5 sm:flex flex-col sm:flex-row">
 		<label class="flex gap-3 mx-4">
 			<input type="checkbox" bind:checked={$showAllBounties} />
