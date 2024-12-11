d1a2191242053a6ba65df86ec9b67d154d58c1bd
Tom Adler <me@arty.name>
2024-12-11 14:38:42 +0100

    no chopsticks page on production (#386)

--- src/routes/chopsticks/+page.server.ts
+++ src/routes/chopsticks/+page.server.ts
@@ -1,1 +1,9 @@
+import { error } from '@sveltejs/kit';
+import { PUBLIC_HIDE_TEST_BAR } from '$env/static/public';
+
 export const ssr = false;
+
+export function load() {
+	if (!PUBLIC_HIDE_TEST_BAR) return {};
+	error(404);
+}
