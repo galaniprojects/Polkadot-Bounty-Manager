9245274e36a2779ccbd5b709e329371b539d83dc
Tom Adler <me@arty.name>
2025-01-14 21:14:03 +0100

    fix import

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,7 +4,7 @@ import { endpoints } from '../utils/endpoints';
 export async function load() {
 	if (typeof window !== 'undefined') {
 		// in browser create the API before rendering page contents
-		const { createTypedApi } = await import('../utils/polkadot');
+		const { createTypedApi } = await import('../utils/createTypedApi');
 		dotApi.set(createTypedApi(endpoints));
 	}
 
