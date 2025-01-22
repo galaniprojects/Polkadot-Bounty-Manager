38b39931a46ea603ba2485328fb20818417420da
Tom Adler <me@arty.name>
2025-01-22 14:52:02 +0100

    initialize once

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,9 @@
 import { dotApi } from '../stores';
 import { endpoints } from '../utils/endpoints';
+import { get } from 'svelte/store';
 
 export async function load() {
-	if (typeof window !== 'undefined') {
+	if (typeof window !== 'undefined' && typeof get(dotApi) === 'undefined') {
 		// in browser create the API before rendering page contents
 		const { createTypedApi } = await import('../utils/createTypedApi');
 		dotApi.set(createTypedApi(endpoints));
