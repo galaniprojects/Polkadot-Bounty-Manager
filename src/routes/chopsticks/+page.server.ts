4770e5b8344c5d9d67f01fb07de3acd1e0651393
Abdulrahim Al Methiab <abdul@kilt.io>
2025-02-06 16:46:13 +0100

    fix chopsticks error

--- src/routes/chopsticks/+page.server.ts
+++ src/routes/chopsticks/+page.server.ts
@@ -1,9 +1,1 @@
-import { error } from '@sveltejs/kit';
-import { PUBLIC_HIDE_TEST_BAR } from '$env/static/public';
-
 export const ssr = false;
-
-export function load() {
-	if (!PUBLIC_HIDE_TEST_BAR) return {};
-	error(404);
-}
