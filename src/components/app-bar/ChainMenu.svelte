c99a2b055f2e4ad4f9b2104a647c1e0185b4e611
Tom Adler <me@arty.name>
2025-02-20 08:48:37 +0100

    toggle endpoint

--- src/components/app-bar/ChainMenu.svelte
+++ src/components/app-bar/ChainMenu.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import { fetchBountiesAndChildBounties } from '../../utils/fetch-bounties';
+	import { initializeApi } from '../../utils/initializeApi';
+	import { endpoints } from '../../utils/endpoints';
 	import Dropdown from '../common/DropdownMenu.svelte';
 	import coin from '../Input/coin.svg';
 	import logoPaseo from './LogoPaseo.svg';
@@ -7,14 +11,34 @@
 	import type { Labeled } from './labeled';
 
 	const chains = [
-		{ label: 'Polkadot', logo: coin, invertedLogo: coinInverted },
-		{ label: 'Paseo', logo: logoPaseo, invertedLogo: paseoInverted }
+		{
+			label: 'Polkadot',
+			logo: coin,
+			invertedLogo: coinInverted,
+			endpoint: endpoints[0]
+		},
+		{
+			label: 'Paseo',
+			logo: logoPaseo,
+			invertedLogo: paseoInverted,
+			endpoint: 'wss://paseo-rpc.dwellir.com'
+		}
 	];
 
-	let selectedChain: Labeled = chains[0];
+	const isPaseoSelected =
+		typeof sessionStorage !== 'undefined' && sessionStorage.getItem('node') === chains[1].endpoint;
+	let selectedChain = isPaseoSelected ? chains[1] : chains[0];
 
-	function handleChainChange(chain: Labeled) {
-		selectedChain = chain;
+	onMount(() => {
+		const isPaseoSelected = sessionStorage.getItem('node') === chains[1].endpoint;
+		selectedChain = isPaseoSelected ? chains[1] : chains[0];
+	});
+
+	async function handleChainChange(chain: Labeled) {
+		selectedChain = chain as typeof selectedChain;
+		sessionStorage.setItem('node', selectedChain.endpoint);
+		await initializeApi([selectedChain.endpoint]);
+		await fetchBountiesAndChildBounties();
 	}
 </script>
 
