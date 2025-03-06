a6b0c40998cc157da7e083c9dd3dfbd260c4660b
BlertaHamza <102804060+BlertaSunCacti@users.noreply.github.com>
2025-03-06 17:18:13 +0100

    Change wallet items colors (#506)
    * Change wallet items colors
    
    * Color switch for consistency
    
    * Adress comments
    
    * Improve on address width
    
    * Small improvements
    
    ---------
    
    Co-authored-by: BlertaGalani <98878525+BlertaGalani@users.noreply.github.com>

--- src/components/app-bar/AccountItem.svelte
+++ src/components/app-bar/AccountItem.svelte
@@ -1,32 +1,45 @@
 <script lang="ts">
-	import { truncateString } from '../../utils/common';
 	import PolkadotIcon from '../common/PolkadotIcon.svelte';
-	import ForwardIcon from '../svg/ForwardIcon.svg';
 	import PeopleChainName from '../PeopleChainName.svelte';
 
 	export let name;
 	export let address: string;
 </script>
 
-<div
-	class="cursor-pointer flex justify-between items-center bg-backgroundButtonDark bg-opacity-20 px-2 s:px-5 py-2 rounded-lg mt-2"
->
-	<!-- Account Identicon and Name -->
-	<div class="flex items-center space-x-3">
-		<div class="w-10 h-10">
-			<PolkadotIcon {address} />
-		</div>
-		<div class="flex flex-col">
-			<span class="self-start">{name}</span>
-			<span class="text-sm whitespace-nowrap">
-				<PeopleChainName {address}>{truncateString(address, 20)}</PeopleChainName>
-			</span>
-		</div>
+<div class="info">
+	<div class="identicon">
+		<PolkadotIcon {address} />
 	</div>
-
-	<!-- Action Button -->
-	<div class="flex items-center space-x-1">
-		<span class="text-base">Select</span>
-		<span class="mb-1"><img src={ForwardIcon} alt="Forward" /></span>
+	<div class="details">
+		{name}
+		<br />
+		<PeopleChainName {address}>{address}</PeopleChainName>
 	</div>
 </div>
+
+<style>
+	.info {
+		display: flex;
+		align-items: center;
+		cursor: pointer;
+		background-color: theme('colors.backgroundButtonLight');
+		gap: 12px;
+		padding: 8px;
+		border-radius: 8px;
+		margin-top: 8px;
+	}
+
+	.identicon {
+		width: 40px;
+		height: 40px;
+		flex-shrink: 0;
+	}
+
+	.details {
+		flex-grow: 1;
+		overflow: hidden;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+		text-align: left;
+	}
+</style>
