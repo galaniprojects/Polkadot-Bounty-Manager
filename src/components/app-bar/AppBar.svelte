37dc5966b92fd295ed8eb89e66cb990106540427
BlertaGalani <98878525+BlertaGalani@users.noreply.github.com>
2025-03-13 14:12:47 +0100

    Add toggle in header

--- src/components/app-bar/AppBar.svelte
+++ src/components/app-bar/AppBar.svelte
@@ -44,11 +44,17 @@
 		activeAccount.set(account);
 		polkadotSigner.set(account.polkadotSigner);
 	}
+
+	let isCuratorViewActive = false;
+
+	function toggleView() {
+		isCuratorViewActive = !isCuratorViewActive;
+	}
 </script>
 
-<header class="relative flex justify-between items-center min-h-20 p-2 md:p-0 md:w-[754px] mx-auto">
+<header class="header">
 	<!-- First Element -->
-	<div class="flex flex-col items-center space-y-3">
+	<div class="first">
 		<a href="/curator-actions">
 			<img width="46" height="30" src={LogoBountyManagerHeader} alt="Logo Bounty Manager" />
 		</a>
@@ -56,6 +62,13 @@
 	</div>
 
 	<!-- Second Element -->
+	<button class="toggleContainer" on:click={toggleView}>
+		<div class="toggleSlider {isCuratorViewActive ? 'curatorViewActive' : ''}"></div>
+		<div class="toggleOption {isCuratorViewActive ? 'inactive' : 'active'}">All Bounties</div>
+		<div class="toggleOption {isCuratorViewActive ? 'active' : 'inactive'}">Curator view</div>
+	</button>
+
+	<!-- Third Element -->
 	<div>
 		{#if !$activeAccount}
 			{#if !page.url.pathname.startsWith('/docs/')}
@@ -63,11 +76,10 @@
 			{/if}
 			<w3m-button></w3m-button>
 		{:else}
-			<!-- User Address -->
-			<div class="flex flex-col items-center space-y-[23px] mt-1">
+			<div class="third">
 				<button
 					type="button"
-					class="flex gap-2 items-center"
+					class="account"
 					on:click={async () => {
 						await navigator.clipboard.writeText($activeAccount.address);
 					}}
@@ -87,3 +99,100 @@
 
 <LoadingModal />
 <LoginModal />
+
+<style>
+	.header {
+		position: relative;
+		display: flex;
+		justify-self: center;
+		justify-content: space-between;
+		align-items: end;
+		min-height: 80px;
+		padding: 8px;
+		width: 100%;
+		margin-top: 10px;
+	}
+	.first {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 12px;
+	}
+	.toggleContainer {
+		position: relative;
+		display: flex;
+		background: theme('colors.backgroundButtonLight');
+		border-radius: 12px;
+		width: 218px;
+		height: 50px;
+		align-items: center;
+		cursor: pointer;
+	}
+
+	.toggleSlider {
+		position: absolute;
+		top: 5px;
+		left: 5px;
+		width: 110px;
+		height: 40px;
+		background: theme('colors.backgroundButtonDark');
+		border-radius: 12px;
+		transition: transform 0.3s ease-in-out;
+	}
+
+	.toggleOption {
+		flex: auto;
+		text-align: center;
+		user-select: none;
+		position: relative;
+		transition: color 0.3s ease-in-out 0.05s;
+	}
+
+	.curatorViewActive {
+		transform: translateX(98px);
+	}
+
+	.active {
+		color: white;
+	}
+
+	.inactive {
+		color: theme('colors.textPrimary');
+		font-size: 14px;
+	}
+
+	.third {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 20px;
+	}
+	.account {
+		display: flex;
+		gap: 8px;
+		align-items: center;
+	}
+
+	@media (min-width: 768px) {
+		.header {
+			width: 754px;
+			padding: 0px;
+		}
+	}
+	@media (max-width: 375px) {
+		.toggleContainer {
+			width: 150px;
+			padding: 2px;
+		}
+		.toggleSlider {
+			width: 70px;
+		}
+		.curatorViewActive {
+			transform: translateX(70px);
+		}
+		.active,
+		.inactive {
+			font-size: 12px;
+		}
+	}
+</style>
