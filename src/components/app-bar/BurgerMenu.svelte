414d4d8f5ae91fcb8dd1a36fd7ac04e6167dc120
BlertaGalani <98878525+BlertaGalani@users.noreply.github.com>
2025-03-21 17:01:07 +0100

    Fix click-away function

--- src/components/app-bar/BurgerMenu.svelte
+++ src/components/app-bar/BurgerMenu.svelte
@@ -4,19 +4,10 @@
 		activeAccountBounties,
 		walletConnect as wcConnection
 	} from '../../stores';
-	import { onMount } from 'svelte';
 	import modalStyles from '../Modal/Modal.module.css';
 
 	let open = false;
 	let container: HTMLDivElement | null = null;
-
-	$: {
-		if (open) {
-			window.addEventListener('click', closeMenuOnOutsideClick);
-		} else {
-			window.removeEventListener('click', closeMenuOnOutsideClick);
-		}
-	}
 
 	async function logOut() {
 		activeAccount.set(undefined);
@@ -28,29 +19,18 @@
 		open = false;
 	}
 
-	function closeMenuOnOutsideClick(event: MouseEvent) {
-		if (!container?.contains(event.target as Node)) {
+	function clickAway({ target, currentTarget }: MouseEvent) {
+		if (target === currentTarget && target instanceof HTMLDivElement) {
 			open = false;
 		}
 	}
-
-	onMount(() => {
-		return () => {
-			window.removeEventListener('click', closeMenuOnOutsideClick);
-		};
-	});
 </script>
 
 <div bind:this={container} class="relative">
 	<!-- Burger Menu Icon -->
-	<button
-		class="h-[42px]"
-		on:click={() => {
-			open = !open;
-		}}
-	>
+	<button class="h-[42px]" onclick={() => (open = !open)}>
 		<span
-			class="material-symbols-rounded items-center rounded-[10px] bg-backgroundButtonLight px-[23px] py-[11px] text-textPrimary"
+			class="material-symbols-rounded items-center rounded-[10px] bg-backgroundButtonLight px-[23px] py-[13px] text-textPrimary"
 		>
 			menu
 		</span>
@@ -58,12 +38,12 @@
 
 	<!-- Menu Overlay -->
 	{#if open}
-		<div class={modalStyles.backdrop}></div>
+		<div class={modalStyles.backdrop} onclick={clickAway} role="presentation"></div>
 		<div
 			class="absolute top-0 right-0 -mt-[11px] flex flex-col items-start z-20 bg-lightGray border shadow-lg p-[5px] rounded-[10px] w-[210px]"
 		>
 			<button
-				on:click={() => {
+				onclick={() => {
 					open = false;
 				}}
 				class="self-end mt-[5px] mb-[13px]"
@@ -75,7 +55,7 @@
 				</span>
 			</button>
 			<a
-				on:click={() => {
+				onclick={() => {
 					open = false;
 				}}
 				href="/bounty-setup"
@@ -87,7 +67,7 @@
 			<hr class="border-cloudGray mb-3 w-full" />
 
 			<a
-				on:click={() => {
+				onclick={() => {
 					open = false;
 				}}
 				href="/user-manual"
@@ -102,7 +82,7 @@
 			{#if $activeAccount}
 				<hr class="border-cloudGray mb-3 w-full" />
 
-				<button on:click={logOut} class="rounded-md flex space-x-2 items-center mb-2">
+				<button onclick={logOut} class="rounded-md flex space-x-2 items-center mb-2">
 					<span class="material-symbols-rounded bg-accent rounded-full p-2 text-white">
 						logout
 					</span>
